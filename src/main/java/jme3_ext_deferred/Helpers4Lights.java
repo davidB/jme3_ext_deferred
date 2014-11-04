e359e9b4377333ae0d634b6784fe02599f7426cf
David Bernard <david.bernard.31@gmail.com>
2014-11-04 19:04:07 +0100

    Helpers to create light geometry

---
+++ src/main/java/jme3_ext_deferred/Helpers4Lights.java
@@ -0,0 +1,77 @@
+package jme3_ext_deferred;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Quad;
+import com.jme3.scene.shape.Sphere;
+
+public class Helpers4Lights {
+	public static String UD_Global = "LightGlobal";
+
+	/**
+	 *
+	 * @param geo a closed concave volume (if not global, else a quad)
+	 * @param color
+	 * @param assetManager
+	 * @return
+	 */
+	public static Geometry asPointLight(Geometry geo, ColorRGBA color, AssetManager assetManager) {
+		Material mat = assetManager.loadMaterial("Materials/deferred/lighting.j3m");
+		mat.setColor("Color", color);
+		if (geo.getUserData(UD_Global) == null) {
+			geo.setUserData(UD_Global, false);
+		}
+		geo.setMaterial(mat);
+		geo.updateGeometricState();
+		geo.updateModelBound();
+		return geo;
+	}
+
+	/**
+	 *
+	 * @param geo a closed concave volume (if not global, else a quad)
+	 * @param color
+	 * @param assetManager
+	 * @return
+	 */
+	public static Geometry asDirectionnalLight(Geometry geo, Vector3f direction, ColorRGBA color, AssetManager assetManager) {
+		Material mat = assetManager.loadMaterial("Materials/deferred/lighting.j3m");
+		mat.setColor("Color", color);
+		mat.setVector3("LightDir", direction);
+		if (geo.getUserData(UD_Global) == null) {
+			geo.setUserData(UD_Global, true);
+		}
+		geo.setMaterial(mat);
+		geo.updateGeometricState();
+		geo.updateModelBound();
+		return geo;
+	}
+
+	public static Geometry newPointLight(String name, float radius, ColorRGBA color, AssetManager assetManager) {
+		Geometry geo = new Geometry(name, new Sphere(16, 16, radius));
+		geo.setUserData(UD_Global, false); // should use volume+stencil or full screen quad
+		return asPointLight(geo, color, assetManager);
+	}
+
+	public static Geometry newSpotLight(String name, float radiusX, float rangeY, ColorRGBA color, AssetManager assetManager) {
+		Geometry geo = new Geometry(name, Helpers4Mesh.newCone(16, rangeY, radiusX));
+		geo.setUserData(UD_Global, false); // should use volume+stencil or full screen quad
+		return asPointLight(geo, color, assetManager);
+	}
+
+	public static Geometry newPointLightGlobal(String name, ColorRGBA color, AssetManager assetManager) {
+		Geometry geo = new Geometry(name, new Quad(0.5f, 0.5f));
+		geo.setUserData(UD_Global, true);
+		return asPointLight(geo, color, assetManager);
+	}
+
+	public static Geometry newDirectionnalLight(String name, Vector3f direction, ColorRGBA color, AssetManager assetManager) {
+		Geometry geo = new Geometry(name, new Quad(0.5f, 0.5f));
+		geo.setUserData(UD_Global, true); // should use volume+stencil or full screen quad
+		return asDirectionnalLight(geo, direction, color, assetManager);
+	}
+
+}
---
+++ src/main/java/jme3_ext_deferred/Helpers4Mesh.java
@@ -0,0 +1,67 @@
+package jme3_ext_deferred;
+
+import java.nio.IntBuffer;
+
+import com.jme3.math.FastMath;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Mesh;
+import com.jme3.scene.Mesh.Mode;
+import com.jme3.scene.VertexBuffer.Type;
+import com.jme3.util.BufferUtils;
+
+public class Helpers4Mesh {
+
+	/**
+	 * Fill index with basic tessellation of a convex polygon of vertices sorted.
+	 * The tessellation algo is very basic : every triangles start from the point 0 + [pointsOffset]
+	 */
+	public static void tessellation0(IntBuffer index, boolean cw, int... vertices) {
+		int nbTriangles = vertices.length - 2;
+		for(int t = 0; t < nbTriangles; t++) {
+			index.put(vertices[0]);
+			if (cw) {
+				index.put(vertices[t+1]);
+				index.put(vertices[t+2]);
+			} else {
+				index.put(vertices[t+2]);
+				index.put(vertices[t+1]);
+			}
+		}
+	}
+
+	public static void tessellation0(IntBuffer index, boolean cw, boolean loop, int verticesBegin, int verticesEnd) {
+		int lg = Math.abs(verticesEnd - verticesBegin) + 1;
+		int inc = (int)Math.signum(verticesEnd - verticesBegin);
+		int[] vertices = new int[lg + (loop? 1: 0)];
+		for(int i = 0; i < lg; i++) {
+			vertices[i] = verticesBegin + inc * i;
+		}
+		if (loop) {
+			vertices[lg] = verticesBegin + inc;
+		}
+		tessellation0(index, cw, vertices);
+	}
+
+	public static Mesh newCone(int samples, float rangeY, float bottomRadius) {
+		Vector3f[] positions = new Vector3f[samples + 1];
+		IntBuffer index = BufferUtils.createIntBuffer((samples+1 + samples ) * 3);
+		positions[0] = new Vector3f(0,0,0);
+		int bottomOffset = 1;
+		float stepSize = FastMath.TWO_PI / samples;
+		for (int i = 0; i < samples; i++) {
+			positions[bottomOffset + i] = new Vector3f(FastMath.cos(stepSize * i) * bottomRadius, -rangeY, FastMath.sin(stepSize * i) * bottomRadius);
+		}
+		// side faces
+		tessellation0(index, false, true, 0, bottomOffset + samples - 1);
+		// bottom faces
+		tessellation0(index, true, false, bottomOffset, bottomOffset + samples - 1);
+		Mesh m = new Mesh();
+		m.setBuffer(Type.Position, 3, BufferUtils.createFloatBuffer(positions));
+		m.setBuffer(Type.Index, 3, index);
+		//m.setPointSize(3);
+		m.setMode(Mode.Triangles);
+		m.updateBound();
+		return m;
+	}
+
+}
