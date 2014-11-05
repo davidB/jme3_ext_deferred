91becb565fbcd984b372fee13aeff9e99319963c
David Bernard <david.bernard.31@gmail.com>
2014-11-05 17:05:05 +0100

    TestStencilAndFB: fix settings

--- src/test/java/jme3test/post/TestStencilAndFB.java
+++ src/test/java/jme3test/post/TestStencilAndFB.java
@@ -47,6 +47,7 @@ import com.jme3.renderer.Camera;
 import com.jme3.renderer.ViewPort;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
+import com.jme3.system.AppSettings;
 import com.jme3.texture.FrameBuffer;
 import com.jme3.texture.FrameBufferHack;
 import com.jme3.texture.Image.Format;
@@ -65,6 +66,7 @@ public class TestStencilAndFB extends SimpleApplication implements ActionListene
 
 	public static void main(String[] args){
 		TestStencilAndFB app = new TestStencilAndFB();
+		app.setSettings(new AppSettings(true));
 		app.start();
 	}
 
